97aaca25aa066f71a432b78e381dc9827f647d05
pervezmia <ahmedpervezkabir@gmail.com>
2026-03-12 16:48:08 +0600

    react first project add

--- milestone-05/Module-30.5 Bonus Local Storage/borwser Storage API/js/cart.js
+++ milestone-05/Module-30.5 Bonus Local Storage/borwser Storage API/js/cart.js
@@ -1,16 +1,40 @@
 const handleAddProducts = () => {
     const productEl = document.getElementById("product");
     const quantityEl = document.getElementById("quatity");
-    const product = productEl.value; 
+    const product = productEl.value;
     const quantity = quantityEl.value;
-    console.log("product add", product, quantity);
+    // console.log("product add", product, quantity);
 
     displayProducts(product, quantity);
+
+    addProductToCart(product, quantity);
 
     productEl.value = "";
     quantityEl.value = "";
 }
 
+const getCart = () => {
+    let cart = {};
+
+    const cartJSON = localStorage.getItem("cart");
+    // console.log(cartJSON);
+    if(cartJSON) {
+        
+    }
+
+
+
+    return cart;
+}
+
+const addProductToCart = (product, quantity) => {
+    const cart = getCart();
+    cart[product] = quantity;
+    console.log("cart", cart);
+
+    const cartJSON = JSON.stringify(cart);
+    localStorage.setItem("cart", cartJSON);
+}
 
 const displayProducts = (product, quantity) => {
     const li = document.createElement('li');
@@ -20,3 +44,21 @@ const displayProducts = (product, quantity) => {
     const ul = document.getElementById("products-container");
     ul.appendChild(li);
 }
+
+
+
+
+
+/* 
+*** To save Object / Array in the Local Storage
+*** 1.Convert the obj to JSON string by using JSON.stringify
+*** 2. Localstorage.setItem()
+*/
+
+
+/* 
+* to get obj / array  from the local storage
+* 1. get the item  the from local storage and it will be in JSON string
+* 2. convert the JSON  string to js object by using JSON.parse
+*
+*/
---
+++ milestone-06/Module-32-State-Effects-Data-Fetching/my-react-app/src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {/* <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Sports></Sports> */}
+      {/* <Pet></Pet> */}
+      {/* <Developer name="Mojumder"></Developer>
+      <Developer expert="javaScript"></Developer> */}
+      <Sport name="Pervez" expert="Football" favo></Sport>
+      <Sport></Sport>
+      <Sport></Sport>
+      <Sport></Sport>
+      <Sport></Sport>
+    </>
+  )
+}
+
+function Sport () {
+  return (
+  <div style={{
+    color: "white",
+    background: "black",
+    padding: '20px',
+    border: '4px solid blue',
+    borderRadius: "20px",
+    fontSize: "bold",
+    marginBottom: "20px"
+
+  }}>
+
+    <p>I am </p>
+    <h1>I expert in </h1>
+    <h3>My favorite game </h3>
+  </div>
+    
+  )
+}
+
+
+// function Person() {
+//   const age = 18;
+//   const name = "Pervez";
+//   return (
+//     <>
+//       <ul>
+//         <li>mango</li>
+//         <li>jackfruits</li>
+//         <li>lichi</li>
+//       </ul>
+//       <Sports></Sports>
+//       <p> I am a Person {age}</p>
+//       <h3>Lorem ipsum dolor sit amet.</h3>
+//     </>
+//   )
+
+// }
+
+// function Sports() {
+//   const cssStyle = {
+//     color: 'red',
+//     fontSize: "32px",
+//     border: '8px solid violet',
+//     borderRadius: "20px",
+//     padding: '20px',
+//     background: 'white'
+//   }
+//   return (
+//     <>
+//       <p style={cssStyle}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sequi sint nemo optio ullam perferendis temporibus, praesentium velit eaque, sunt, aperiam quos obcaecati?</p>
+//     </>
+//   )
+// }
+
+// function Pet () {
+//   return (
+//     <>
+//       <p className='style-of-card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nobis voluptatum quia. Illo, veritatis, voluptas, fuga exercitationem maxime dolorum enim eos quis eligendi eum et vel veniam cum! Sed iure, officiis quisquam ex accusantium laboriosam eveniet totam quidem velit beatae rem excepturi cumque, nemo suscipit praesentium. Libero architecto similique impedit nulla officiis sunt quas atque officia provident! Ipsum mollitia, distinctio nostrum voluptas, impedit tempora voluptate quaerat aperiam expedita sequi quia ullam accusamus alias quos eaque vel nulla cupiditate repellat tempore consectetur provident delectus! Maiores vel aliquid commodi provident dicta hic consectetur, sunt placeat adipisci vero, veniam nam molestias id facilis, beatae deserunt delectus architecto. Earum omnis consequatur vero illo deleniti odio temporibus perferendis facere? Aperiam cumque adipisci et voluptatem consectetur asperiores velit facere vel? Ad a omnis ea officiis consequuntur saepe. Voluptatum esse nemo, reiciendis corporis deleniti quos nisi at quod non aliquid adipisci impedit aspernatur debitis error soluta nulla delectus explicabo, ipsam nobis. Pariatur ab ex vitae repellendus maiores natus asperiores nemo illo delectus, nam consectetur neque eius corrupti? Eius cumque labore suscipit aut ratione. Ipsa minus enim eveniet reiciendis odio error laborum doloremque mollitia. Iste quidem dolores rerum aperiam ducimus dolore beatae animi? Dolore corporis quas cupiditate ullam vero cumque, expedita, quia nam ipsam fuga cum sapiente aliquam provident, aliquid accusantium laborum soluta? Tenetur molestias at aliquam qui atque accusamus ipsa iusto illo debitis, doloribus, molestiae nam perferendis. Consectetur eveniet, sit fuga veniam ipsum nesciunt neque itaque voluptate amet asperiores. Voluptatibus dignissimos aspernatur quasi ex aliquam quia id exercitationem, explicabo ducimus necessitatibus obcaecati fugiat atque assumenda veritatis repellendus laudantium! Amet, veniam molestias. Provident eligendi autem nobis animi! Aliquam nam facere accusamus recusandae voluptas ex nihil. Illo, libero ullam consectetur consequuntur nihil eveniet, repellendus a distinctio doloribus, necessitatibus quidem at maxime beatae dolor voluptas commodi quis eum eius enim ad ex non voluptatem soluta. Neque odio labore consequuntur debitis dolorum minus minima laudantium obcaecati possimus harum perferendis mollitia, quasi earum sed, tempora reprehenderit unde accusantium pariatur illum quis totam repellendus? Molestiae, culpa dolores eum modi quia dolorum dolor aut eligendi voluptatum laboriosam minima debitis facere inventore quibusdam corrupti accusamus quisquam voluptatibus adipisci iusto numquam quo! Repellendus, nesciunt explicabo ipsum facilis iusto magnam. Quisquam sunt magnam fugiat modi similique repellendus dolore. Fugit, laudantium aut, nisi fuga blanditiis eveniet quas recusandae neque deleniti perferendis dolor inventore enim magni id ipsam perspiciatis dolorum. Debitis ratione aliquam culpa quia quam, cumque asperiores nemo.</p>
+//     </>
+//   )
+// }
+
+// function Developer(props) {
+//   console.log(props);
+
+//   const personStyle = {
+//     color: "red",
+//     textAlign: "right"
+//   }
+//   return(
+//     <div style={personStyle}>
+//       <h3>I expart in {props.expert}</h3>
+//       <p>I am {props.name}</p>
+//     </div>
+//   )
+  
+ 
+// }
+
+export default App
