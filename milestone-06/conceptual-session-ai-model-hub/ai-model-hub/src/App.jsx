effc703a7e12a72711509d879f418eeca6003e8f
pervezmia <ahmedpervezkabir@gmail.com>
2026-03-29 06:03:26 +0600

    fix: cleaned nested repo

---
+++ milestone-06/conceptual-session-ai-model-hub/ai-model-hub/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h3 className=''>Alhamdulillah</h3>
+      <div className='flex justify-center items-center'>
+        <button className="btn btn-secondary">Secondary</button>
+      </div>
+    </>
+  )
+}
+
+export default App
