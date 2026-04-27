cdf4eb04dc219af3a1b5bf9d1e0b7c86bc84623f
pervezmia <ahmedpervezkabir@gmail.com>
2026-04-27 23:03:21 +0600

    mongobd added

--- milestone-08/module-46/dragon-news/src/app/(auth)/signin/page.jsx
+++ milestone-08/module-46/dragon-news/src/app/(auth)/signin/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 const SignInPage = () => {
-  const [seePassword, setSeePassword] = useState(true);
+  const [seePassword, setSeePassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -13,28 +14,31 @@ const SignInPage = () => {
     formState: { errors },
   } = useForm();
 
-  const loginHandle = (data) => {
+  const signUpHandle = (data) => {
     // e.preventDefault();
     // const mail = e.target.email.value;
     // const password = e.target.password.value;
     // console.log(mail, password);
     console.log(data);
+    const {email, password} = data;
+
   };
   console.log(errors);
   return (
     <div className="container  mx-auto bg-slate-100 min-h-[80vh] flex justify-center items-center">
       <div className="p-10 rounded-xl bg-white">
         <h2 className="font-bold mb-6">Login your account</h2>
-        <form className="space-y-3.5" onSubmit={handleSubmit(loginHandle)}>
+        <form className="space-y-3.5" onSubmit={handleSubmit(signUpHandle)}>
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Email</legend>
             <input
               type="email"
               className="input"
               placeholder="Type here email"
-              defaultValue="test"
-              {...register("email")}
+              
+              {...register("email", { required: "Email field is required" })}
             />
+             {errors.email && <span className="text-red-600">{errors.email.message}</span>}
           </fieldset>
 
 
@@ -44,15 +48,19 @@ const SignInPage = () => {
               type={seePassword ? "text" : "password"}
               className="input"
               placeholder="Type here password"
-              defaultValue="test"
               {...register("password", { required: "Password field is required" })}
               />
               <span className="absolute top-4.5 right-4 cursor-pointer" onClick={() => setSeePassword(!seePassword)}>{seePassword ? <IoMdEye /> : <IoMdEyeOff />}</span>
           {errors.password && <span className="text-red-600">{errors.password.message}</span>}
           </fieldset>
 
-          <button className="btn w-full bg-gray-700 text-white">Login</button>
+          <button className="btn w-full bg-gray-700 text-white">Log In</button>
+
         </form>
+        <p className="text-xs">
+          Don't have an account ? {""}
+          <Link href={"/signup"} className="text-blue-500">Sign up</Link>
+        </p>
       </div>
     </div>
   );
--- milestone-08/module-46/dragon-news/src/app/(auth)/signup/page.jsx
+++ milestone-08/module-46/dragon-news/src/app/(auth)/signup/page.jsx
@@ -1,11 +1,113 @@
-import React from 'react';
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 const SignUpPage = () => {
-    return (
-        <div>
-            <h1>sign up page</h1>
-        </div>
-    );
+  const [seePassword, setSeePassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const loginHandle = async (data) => {
+    console.log(data);
+    const {name, email, photo, password} = data;
+    console.log(email, photo);
+
+    const {data: res, error} = await authClient.signUp.email({
+        Name: name,
+        Email: email,
+        Password: password,
+        Photo: photo,
+        callbackURL: "/",
+    })
+
+    console.log(res, error);
+  };
+  console.log(errors);
+
+  return (
+    <div className="container  mx-auto bg-slate-100 min-h-[80vh] flex justify-center items-center">
+      <div className="p-10 rounded-xl bg-white">
+        <h2 className="font-bold mb-6">Login your account</h2>
+        <form className="space-y-3.5" onSubmit={handleSubmit(loginHandle)}>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Your Name</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Type here name"
+              {...register("name", { required: "Name field is required" })}
+            />
+            {errors.name && (
+              <span className="text-red-600">{errors.name.message}</span>
+            )}
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Photo URL</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Type here email"
+              {...register("photo", { required: "URL field is required" })}
+            />
+            {errors.photo && (
+              <span className="text-red-600">{errors.photo.message}</span>
+            )}
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="email"
+              className="input"
+              placeholder="Type here email"
+              {...register("email", { required: "Email field is required" })}
+            />
+            {errors.email && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
+          </fieldset>
+
+          <fieldset className="fieldset relative">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type={seePassword ? "text" : "password"}
+              className="input"
+              placeholder="Type here password"
+              {...register("password", {
+                required: "Password field is required",
+              })}
+            />
+            <span
+              className="absolute top-4.5 right-4 cursor-pointer"
+              onClick={() => setSeePassword(!seePassword)}
+            >
+              {seePassword ? <IoMdEye /> : <IoMdEyeOff />}
+            </span>
+            {errors.password && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+          </fieldset>
+
+          <button className="btn w-full bg-gray-700 text-white">Sign Up</button>
+        </form>
+        <p className="text-xs">
+          Already have an account?  {""}
+          <Link href={"/signin"} className="text-blue-500">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
